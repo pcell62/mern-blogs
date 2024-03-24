e26b34ca63e94e3fea39d065297fa83782250343
Priyanshu Kumar <priyanshukumr10714@gmail.com>
2024-03-24 14:24:48 +0530

    Add PrivateRoute component to protect dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,22 @@ import Dashboard from "./pages/Dashboard";
 
 import Header from "./components/Header";
 import FooterComp from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <div className="max-w-[88rem] m-auto"> */}
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
       <FooterComp />
-      {/* </div> */}
     </BrowserRouter>
   );
 }
